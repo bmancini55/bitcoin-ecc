f36eda14a8020b2e6bdee26122af382ca8d23e7e
bmancini55 <bmancini@gmail.com>
2021-11-12 11:03:51 -0500

    add comments to Schnorr

--- lib/Schnorr.ts
+++ lib/Schnorr.ts
@@ -6,29 +6,44 @@ import { CurvePoint } from "./CurvePoint";
 import { mod, pow } from "./util/BigIntMath";
 import { SchnorrSig } from "./SchnorrSig";
 
-function hash(tag: string, ...value: Buffer[]): Buffer {
-    const tagHash = sha256(Buffer.from(tag, "utf8"));
-    return sha256(combine(tagHash, tagHash, ...value));
-}
-
-function lift(x: bigint): CurvePoint {
-    const p = Secp256k1.P;
-    const f = Secp256k1.field;
-
-    // c=x^3+7
-    const c = f.add(f.pow(x, 3n), 7n);
-
-    // sqrt(c) = c^((p+1)/4)
-    const y = f.sqrt(c);
-
-    if (c !== f.pow(y, 2n)) {
-        throw new Error("liftX failed");
-    }
-
-    return new CurvePoint(Secp256k1, x, y % 2n === 0n ? y : p - y);
-}
-
+/**
+ * Implements signing and verifing Schnorr signatures as implemented in
+ * BIP340.
+ */
 export class Schnorr {
+    /**
+     * Signs a message using the BIP340 implementation of Schnorr
+     * signatures. Generally the Schnorr signature algorithm is:
+     *
+     * ```
+     * sign(e, m) where
+     *  e = secret
+     *  m = hash of the message
+     *
+     *  G = curve generator
+     *  P = e*G
+     *  k = some randomly unguessable value
+     *  R = k*G
+     *  r = R.x
+     *  c = hash(R.x || P.x || m)
+     *  s = k + c*e
+     *
+     * sig=(r,s)
+     * ```
+     *
+     * Several optimizations:
+     * - An even-y value is forced to prevent inherent mutability of
+     * signature via the two valid signature values: (s, n-s). It is
+     * forced by using the even-y of the `e` and `k` values
+     * - k is protected from reuse via an auxiliary entropy source `aux`
+     * that is combined with the public key point and message
+     *
+     *
+     * @param sk secret key (32-bytes)
+     * @param m message (32-bytes)
+     * @param a auxliary data (32-bytes)
+     * @returns
+     */
     public static sign(sk: Buffer, m: Buffer, a: Buffer): SchnorrSig {
         const f = Secp256k1.field;
 
@@ -87,6 +102,33 @@ export class Schnorr {
         return sig;
     }
 
+    /**
+     * Verifies a Schnorr signature according to the generalized
+     * formula:
+     *
+     * ```
+     * verify(r, s, P, m)
+     *
+     *  c = hash(r || P.x || m)
+     *  s = k + c*e
+     *  sG = kG + ceG
+     *  sG = R + cP
+     *  R = sG - cP
+     *
+     * verify R.r = r
+     * ```
+     *
+     * Addiontionally BIP340 requires that we validate the R.y value is
+     * even to prevent the inherent mutability of signatures.
+     *
+     * The supplied public key is a 32-byte value. The even y-value
+     * public key is used.
+     *
+     * @param pk 32-byte private key (x-value)
+     * @param m 32-byte message that was signed
+     * @param sig 64-byte signature containing (r,s) tuple
+     * @returns true if the signature is valid
+     */
     public static verify(pk: Buffer, m: Buffer, sig: SchnorrSig): boolean {
         if (pk.length !== 32) {
             throw new Error("Invalid public key for Schnorr verification");
@@ -136,4 +178,44 @@ export class Schnorr {
         // otherwise return true
         return true;
     }
+}
+
+/**
+ * Performs a tagged sha256 hash of the supplied data.  For example:
+ *
+ * hashBIP0340/aux(a)=sha256(BIP0340/aux||a)
+ * @param tag
+ * @param value
+ * @returns
+ */
+function hash(tag: string, ...value: Buffer[]): Buffer {
+    const tagHash = sha256(Buffer.from(tag, "utf8"));
+    return sha256(combine(tagHash, tagHash, ...value));
+}
+
+/**
+ * Generates a CurvePoint at x with an even y.  This function solves
+ * for y and returns the y value that is even of the pair (y, p-y).
+ *
+ * I order to do this we solve for y in the equation y^2=x^3+7. Then
+ * because P%4=3 we are able to solve for the square root of y via the
+ * equation c^((p+1)/4).
+ * @param x
+ * @returns
+ */
+function lift(x: bigint): CurvePoint {
+    const p = Secp256k1.P;
+    const f = Secp256k1.field;
+
+    // c=x^3+7
+    const c = f.add(f.pow(x, 3n), 7n);
+
+    // sqrt(c) = c^((p+1)/4)
+    const y = f.sqrt(c);
+
+    if (c !== f.pow(y, 2n)) {
+        throw new Error("liftX failed");
+    }
+
+    return new CurvePoint(Secp256k1, x, y % 2n === 0n ? y : p - y);
 }
